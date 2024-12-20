d07cc5e3e4ffafd1906c27f5ee3bc777a18365df
Domagoj Hegedušić <dhegedusi21@foi.hr>
2024-12-20 13:06:27 +0100

    Add project files.

---
+++ Simple_Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Simple_Blog.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
